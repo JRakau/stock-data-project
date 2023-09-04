bd6d08870892989192cce4223d61dfdebfe8a82e
Josue Rakauskas <josuerakauskas@josues-air.home>
2023-09-04 19:44:14 +0100

    removed initial files for test, and create main.py, with first function to test the API

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import time
+
+# dX4gER*E6GN!yEZ */
+
+
+ticker = "AAPL"
+api_key = "74fee5c279404d629efab122626502bb"
+
+
+def get_stock_quote(ticker_symbol, api):
+    url = f"https://api.twelvedata.com/quote?symbol={ticker_symbol}&apikey={api}"
+    response = requests.get(url).json()
+    return response
+
+
+stockdata = get_stock_quote(ticker, api_key)
+
+#
+# Construct
+# --
+
+print(stockdata)
