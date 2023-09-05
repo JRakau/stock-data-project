c711c3d116015b9decd75c9528464111f2626ffc
Josue Rakauskas <josuerakauskas@Josues-MacBook-Air.local>
2023-09-05 20:02:50 +0100

    Created the main() function responsable to run all funcitons

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ api_key = "74fee5c279404d629efab122626502bb"
 
 
 def get_stock_quote(ticker_symbol, api):
+
     url = f"https://api.twelvedata.com/quote?symbol={ticker_symbol}&apikey={api}"
     response = requests.get(url).json()
     return response
 
 
-#
-# Construct
-# --
+def main():
+    """
+    Main routine for app
+    It is responsable to run all program functions
+
+    """
+
+
 ticker = input("Enter your ticker symbol here:\n")
 
 
@@ -24,3 +30,7 @@ stockdata = get_stock_quote(ticker, api_key)
 
 print(stockdata)
 print(stockdata['symbol'])
+
+print("Welcome to Your Stocks")
+
+main()
